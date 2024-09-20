a34adaf199ef54afcb240e3fae1dc68084485393
Code-By-Denis <codebydenis@gmail.com>
2024-09-20 23:51:05 +0300

    Initial commit with solution and projects

---
+++ MovieWave.DAL/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieWave.DAL;
+
+public class AppDbContext : DbContext
+{
+	public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+	{
+		Database.EnsureCreated();
+	}
+}
