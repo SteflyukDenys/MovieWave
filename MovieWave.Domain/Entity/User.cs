cae262e811963c69c10a36943d0422927c980e9d
Code-By-Denis <codebydenis@gmail.com>
2024-09-21 01:17:06 +0300

    Create User.cs

---
+++ MovieWave.Domain/Entity/User.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using MovieWave.Domain.Enum;
+
+namespace MovieWave.Domain.Entity
+{
+	public class User : IdentityUser<Guid>
+	{
+		public UserRole UserRole { get; set; }
+
+		public string Login { get; set; }
+
+		public string? AvatarPath { get; set; }
+		public string? BackdropPath { get; set; }
+		public Gender? Gender { get; set; }
+		public string? Description { get; set; }
+		public DateTime? Birthday { get; set; }
+		public DateTime? LastSeenAt { get; set; }
+
+		// One-to-Many
+		public ICollection<UserMediaItemList> UserMediaItemLists { get; set; }
+		public ICollection<Comment> Comments { get; set; }
+		public ICollection<UserSubscription> UserSubscriptions { get; set; }
+		public ICollection<Notification> Notifications { get; set; }
+		public ICollection<Review> Reviews { get; set; }
+	}
+}
