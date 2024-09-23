ff84b354f36615ea91d6b6b3a2822bdd71b7c769
Code-By-Denis <codebydenis@gmail.com>
2024-09-23 17:04:18 +0300

    Configured the connection string for SQL Server using `secrets.json`.

--- MovieWave.DAL/AppDbContext.cs
+++ MovieWave.DAL/AppDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
 
 namespace MovieWave.DAL;
 
@@ -6,6 +8,17 @@ public class AppDbContext : DbContext
 {
 	public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
 	{
+		Database.EnsureDeleted();
 		Database.EnsureCreated();
+	}
+
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
+	}
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 	}
 }
---
+++ MovieWave.DAL/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MovieWave.DAL.DependencyInjection;
+
+public static class DependencyInjection
+{
+	public static void AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
+	{
+		var connectionString = configuration.GetConnectionString("SQLServer");
+
+		services.AddDbContext<AppDbContext>(options =>
+		{
+			options.UseSqlServer(connectionString);
+		});
+	}
+}
